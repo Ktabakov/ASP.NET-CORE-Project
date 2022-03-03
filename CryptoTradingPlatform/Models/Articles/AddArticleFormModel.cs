cf36aaf4e357cb05788909d5f9e23882883a428b
Ktabakov <ktabakov2140@gmail.com>
2022-03-03 13:11:11 +0100

    Forms Format fixed

--- CryptoTradingPlatform/Models/Articles/AddArticleFormModel.cs
+++ CryptoTradingPlatform/Models/Articles/AddArticleFormModel.cs
@@ -1,9 +1,12 @@
-﻿namespace CryptoTradingPlatform.Models.Articles
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CryptoTradingPlatform.Models.Articles
 {
     public class AddArticleFormModel
     {
         public string Content { get; set; }
 
+        [Display(Name = "Image URL")]
         public string ImageURL { get; set; }
     }
 }
--- CryptoTradingPlatform/Models/Articles/ArticleViewModel.cs
+++ CryptoTradingPlatform/Models/Articles/ArticleViewModel.cs
@@ -1,9 +1,12 @@
-﻿namespace CryptoTradingPlatform.Models.Articles
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CryptoTradingPlatform.Models.Articles
 {
     public class ArticleViewModel
     {
         public string Content { get; set; }
 
+        [Display(Name = "Image URL")]
         public string ImageURL { get; set; }
     }
 }
