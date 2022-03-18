e604c15b6f8d3e7f3b2bcc69aa8139bb51bb84c3
Ktabakov <ktabakov2140@gmail.com>
2022-03-18 13:13:08 +0100

    CacheModel Added - maybe use for api calls

---
+++ CryptoTradingPlatfrom.Core/Cache/CacheModel.cs
@@ -0,0 +1,32 @@
+﻿using CryptoTradingPlatform.Core.Models.Articles;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace CryptoTradingPlatfrom.Core.Cache
+{
+    public static class CacheModel
+    {
+        private static IMemoryCache memoryCache = new MemoryCache(new MemoryCacheOptions());
+
+        public static void Add(string cacheKey, List<ArticleViewModel> model)
+        {
+            var cacheExpiryOptions = new MemoryCacheEntryOptions()
+            {
+                AbsoluteExpiration = DateTime.Now.AddSeconds(30),
+                Priority = CacheItemPriority.High,
+                SlidingExpiration = TimeSpan.FromSeconds(20),
+            };
+            memoryCache.Set(cacheKey, model, cacheExpiryOptions);
+        }
+
+        public static List<ArticleViewModel> Get(string cacheKey)
+        {
+            var result = memoryCache.Get(cacheKey);
+            return (List<ArticleViewModel>)result;
+        }
+
+        public static void Delete(string cacheKey)
+        {
+            memoryCache.Remove(cacheKey);
+        }
+    }
+}
